7dd12ec1fa9441085907be421547e05529e80816
Kumar Shubham <kumar.shubham@coditas.com>
2021-01-08 19:12:11 +0530

    Added style through module.css in Page2

---
+++ pages/page2.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import rando from "../styles/Page2.module.css"
+
+function page2() {
+    return (
+        <div className={rando.container}>
+            <h1>Hi </h1>
+            <h1>There</h1>
+        </div>
+    )
+}
+
+export default page2
