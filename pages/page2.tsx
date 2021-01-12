b299efd85c2ddba6ea36a3760ec0d6120c769e1c
Kumar Shubham <kumar.shubham@coditas.com>
2021-01-12 20:12:30 +0530

    Added useRouter

--- pages/page2.tsx
+++ pages/page2.tsx
@@ -1,11 +1,18 @@
 import React from 'react'
 import styles from "../styles/Page2.module.css"
+import {useRouter} from "next/router"
 
 function page2() {
+    const router=useRouter()
+    console.log(router)
+    const handleClick=()=>{
+            router.push("/")
+    }
     return (
         <div className={styles.container}>
             <h1>Hi </h1>
             <h1>There</h1>
+            <button onClick={handleClick}>Home</button>
         </div>
     )
 }
